be5d834c972db65301a1c98b324286818829d42a
Jun Chow <junzhou7355608@gmail.com>
2025-07-16 09:29:26 +0800

    feat: add antd-mobile and antd-mobile-icons dependencies, implement TabBar component with routing, and adjust global CSS for tab bar height

--- apps/admin/src/components/tab-bar/index.tsx
+++ apps/admin/src/components/tab-bar/index.tsx
@@ -1,3 +1,48 @@
+import { AppstoreOutline, FolderOutline, ShopbagOutline, UserOutline } from 'antd-mobile-icons'
+import {
+  useNavigate,
+  useLocation,
+} from 'react-router-dom'
+import {  TabBar as AntdTabBar } from 'antd-mobile'
+
 export default function TabBar() {
-  return <div className="fixed bottom-0 left-0 right-0 m-auto h-tabbar bg-white">TabBar</div>
+
+  const navigate = useNavigate()
+  const location = useLocation()
+  const { pathname } = location
+
+  const setRouteActive = (value: string) => {
+    navigate(value)
+  }
+
+  const tabs = [
+    {
+      key: '/',
+      title: '首页',
+      icon: <AppstoreOutline />,
+    },
+    {
+      key: '/order',
+      title: '订单',
+      icon:<FolderOutline />,
+    },
+    {
+      key: '/goods',
+      title: '商品',
+      icon: <ShopbagOutline />,
+    },
+    {
+      key: '/mine',
+      title: '我的',
+      icon: <UserOutline />,
+    },
+  ]
+
+  return <div className="w-full md:w-[430px] fixed bottom-0 left-0 right-0 m-auto h-tabbar bg-white flex justify-center items-center">
+    <AntdTabBar className='w-full' activeKey={pathname} onChange={value => setRouteActive(value)}>
+      {tabs.map(item => (
+        <AntdTabBar.Item key={item.key} icon={item.icon} title={item.title} />
+      ))}
+    </AntdTabBar>
+  </div>
 }
