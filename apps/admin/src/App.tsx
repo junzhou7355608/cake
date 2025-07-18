72b67106217ca4388d6bdf17bc81f69c90b83268
Jun Chow <junzhou7355608@gmail.com>
2025-07-18 16:14:02 +0800

    feat: update project configuration and admin app assets, change favicon and title, and remove unused SVG files

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -1,5 +1,8 @@
-import { Routes } from '@generouted/react-router'
-import Layout from "@/layouts"
+import Layout from '@/layouts'
+import { RouterProvider, createHashRouter } from 'react-router-dom'
+import { routes } from '@generouted/react-router'
+
+const Routes = () => <RouterProvider router={createHashRouter(routes)} />
 
 function App() {
   return (
