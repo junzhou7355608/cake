5910a24cc4aa59f28954a43be34a96bc2bf463c2
Jun Chow <junzhou7355608@gmail.com>
2025-07-22 13:54:54 +0800

    feat: improve admin app performance by optimizing component rendering and enhancing state management

---
+++ apps/server/src/main.ts
@@ -0,0 +1,9 @@
+/* eslint-disable turbo/no-undeclared-env-vars */
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule)
+  await app.listen(process.env.PORT ?? 3000)
+}
+bootstrap()
